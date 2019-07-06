7125b7da11fecffe289effad1f7d733c647f854b
Gwamaka Charles <gwaxart19@gmail.com>
2019-07-06 14:48:55 +0300

    Added the pagination functionality as well as a custom 404 template.

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '89@c-mfxdq*g&dblai=2earp(odbe6z7@_sc-sj+zdy(swzc1)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
