b940ddd67a54f6a6c556940690ee3b875f6e8952
Gwamaka Charles <gwaxart19@gmail.com>
2019-07-10 21:13:15 +0300

    Created the vote model.

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 
 
 
@@ -101,4 +101,15 @@ class Role(models.Model):
         unique_together = ('movie', 'person', 'name')
 
 
+class Vote(models.Model):
+    UP = 1
+    DOWN = -1
+    VALUE_CHOICES = ((UP, 'thumb up',),(DOWN, 'thumb down',),)
 
+    value = models.SmallIntegerField(choices=VALUE_CHOICES,)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    voted_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'movie')
